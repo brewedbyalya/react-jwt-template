67cd9cc1786dbfdc6cac32ac8e424b51ec98cb05
alya <aliya2002mah@gmail.com>
2025-08-06 14:24:38 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import {Route , Routes} from 'react-router' ;
-import SignUpForm from './components/SignUpForm/SignUpForm'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import SignUp from './components/SignUp/SignUp'
+import SignIn from './components/SignIn/SignIn'
+import { Route, Routes } from 'react-router'
+import * as authService from './services/authService.js'
+import { useState } from 'react'
 
 const App = () => {
-  
+
+  const initialState = authService.getUser()
+
+  const [user, setUser] = useState(initialState)
+
+  const handleSignUp = async (formData) => {
+   const res = await authService.signUp(formData)
+   setUser(res)
+  }
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token')
+    setUser(null)
+  }
+
+  const handleSignIn = async (formData) => {
+    const res = await authService.signIn(formData)
+    setUser(res)
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar user={user} handleSignOut={handleSignOut} />
       <Routes>
-     <Route path= '/' element={<h1>Hello world.</h1>}/>
-     <Route path='sign-up' element= {<SignUpForm/>} />
-     <Route path= '*' element={<h1>404</h1>}/>
-     </Routes>
+          <Route path='/' element={<h1>Hello world!</h1>} />
+          <Route path='/sign-up' element={<SignUp handleSignUp={handleSignUp} />} />
+          <Route path='/sign-in' element={<SignIn handleSignIn={handleSignIn} />} />
+          <Route path='*' element={<h1>404</h1>} />
+    </Routes>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router";
-const NavBar = () => {
+import { Link } from 'react-router'
+
+const NavBar = (props) => {
+
+
+
   return (
     <nav>
-      <Link to='/'> Home </Link>
-      <Link to='/sign-up'>Sign Up</Link>
+      {props.user ? (
+        <ul>
+          <li>Welcome {props.user.username}</li>
+          <li><Link to="/"> Home </Link></li>
+          <li><Link to='/' onClick={props.handleSignOut}>Sign Out</Link></li>
+        </ul>
+        ) : (
+          <ul>
+            <li><Link to="/"> Home </Link></li>
+            <li><Link to="/sign-up">Sign Up</Link></li>
+            <li><Link to="/sign-in">Sign In</Link></li>
+          </ul>
+          ) }
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar 
